d9d57bd398f3e8a9512d5ce8c917c7a4ad7e0add
ROGUH <hugo@roguh.com>
2024-03-20 22:52:43 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# Local install with:
+# DATA=$HOME/.local/share BIN=$HOME/.local/bin ./install.sh  
+# Global install with:
+# sudo ./install.sh
 set -ex
 
-BIN=/usr/bin/
-DATA=/usr/share/gameoflife
+BIN="${BIN:-/usr/bin/}"
+DATA="${DATA:-/usr/share/gameoflife}"
 
 cp gameoflife.py gameoflife_screensaver "$BIN"
 mkdir -p "$DATA"
